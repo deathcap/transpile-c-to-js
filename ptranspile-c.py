b25dc88c8d20d0c34d9bc01cb5fabbb189a70aba
deathcap <deathcap@gmx.co.uk>
2016-05-03 22:57:50 -0700

    Add some defines to pycparser cpp preprocessor to get further parsing BSD headers

--- ptranspile-c.py
+++ ptranspile-c.py
@@ -1,10 +1,35 @@
 
+import sys
+sys.path.extend(['.', 'pycparser'])
+
 # sudo easy_install pycparser
 from pycparser import parse_file, c_generator
 
+
 def translate(filename):
-    ast = parse_file(filename, use_cpp=True)
+    ast = parse_file(filename,
+        use_cpp=True,
+        cpp_path='gcc',
+        cpp_args=[
+            "-E",
+            "-D__FBSDID(x)=", # FreeBSD identifier
+            "-D__attribute__(x)=", # attribute extension
+            "-D__builtin_va_list=void*", # include/x86/_types.h:154 typedef __builtin_va_list   __va_list; 
+            "-D__inline=",
+            "-D__asm(x)=",
+
+            "-D_RUNETYPE_H_=1", # skip include/runtype.h
+            "-D_RuneLocale=void*", # but it defines this type
+
+            "-D_Noreturn=",
+
+            "-U__BLOCKS__", # no (^) syntax: include/stdlib.h: int  atexit_b(void (^)(void));
+
+            "-nostdinc",
+            "-Iinclude", # copy from /usr/include
+            #"-Ipycparser/utils/fake_libc_include",
+            ])
     generator = c_generator.CGenerator()
     print(generator.visit(ast))
 
-translate('hello.c')
+translate(sys.argv[1])
